91ee50810d819600f1aa0c583c32b9477e3e610e
lchouville <luka.chouville@laposte.net>
2026-03-03 15:42:48 +0100

    develop/WIP Route

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+function Register() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Register
+      </h1>
+    </div>
+  );
+}
+
+export default Register;
